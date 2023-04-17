1815e8e6c7c6e837abacfb15a557e6a6a2340965
Eric Collard <ruffyreborn@gmail.com>
2023-04-16 21:10:18 -0500

    started assignment

---
+++ functionsExercise.js
@@ -0,0 +1,33 @@
+br = (questionNumber) => console.log('='.repeat(25), `\nQUESTION ${questionNumber}`);
+
+br('1');
+// QUESTION 1
+function fozzleBear(){
+    console.log('Wocka Wocka!');
+}
+fozzleBear();
+
+
+br('2');
+// QUESTION 2
+function beaker(speak){
+    console.log(speak);
+    console.log(speak);
+    console.log(speak);
+    console.log(speak);
+}
+beaker('meep');
+
+
+br('3');
+// QUESTION 3
+muppetShow = (a, b) => {
+    if (a === 'Muppet' && b === 'Show'){
+        console.log('It\'s time to play the music. It\'s time to light the lights.');
+    }
+}
+muppetShow('Muppet', 'Show');
+
+
+br('4');
+// QUESTION 4
